bd303a5841f9a94bc368580f722ab24f8e5226ed
DannyAWahby <danielashrafgerges@gmail.com>
2020-08-09 15:25:42 +0200

    Initial Commit

---
+++ SimpleWeatherAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleWeatherAPI
+//
+//  Created by Daniel Wahby on 8/9/20.
+//  Copyright © 2020 Daniel Radtke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
