693a28c228a8b8839f8faba57fa8982f43a3b43c
daniel.ashraf@imperialstud.io <daniel.ashraf@imperialstud.io>
2021-04-10 18:10:41 +0200

    Initial Commit

---
+++ Weatherify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weatherify
+//
+//  Created by Daniel Wahby on 10/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
