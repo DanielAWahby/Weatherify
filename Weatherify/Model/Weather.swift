463a3a2cf137b8cec403e150f01c663e67395f34
DanielAGWahby <danielashrafgerges@icloud.com>
2022-03-02 17:55:57 +0200

    Finalized Initial Project

--- Weatherify/Model/Weather.swift
+++ Weatherify/Model/Weather.swift
@@ -6,12 +6,50 @@
 //  Copyright © 2020 Daniel Radtke. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 struct Weather: Codable {
     var request:WeatherRequest?
     var location:WeatherLocation?
     var current:CurrentWeather?
     
+    func getFullLocation()->String?{
+        return "\(location?.name ?? ""), \(location?.country ?? "")"
+    }
     
+    func getTemperatureString()->String{
+        return "\(current?.temperature ?? 0)°"
+    }
+    func getWeatherIcon()->UIImage?{
+        if let weatherDescription = current?.weather_descriptions?.first{
+            print(weatherDescription)
+            if weatherDescription.contains("partly"){
+                    return UIImage(named:"Partly Cloudy")
+            }
+            else if weatherDescription.contains("sunny"){
+                if weatherDescription.contains("cloud"){
+                    return UIImage(named:"Partly Cloudy")
+                }
+                return UIImage(named:"Sunny")
+            }
+            else if weatherDescription.contains("fog") || weatherDescription.contains("haze"){
+                return UIImage(named:"Foggy")
+            }
+            else if weatherDescription.contains("thunder") ||  weatherDescription.contains("lightning"){
+                return UIImage(named:"Lightning")
+            }
+            else if weatherDescription.contains("snow"){
+                return UIImage(named:"Snowy")
+            }
+            else if weatherDescription.contains("rain"){
+                return UIImage(named:"Rainy")
+            }
+            else{
+                return UIImage(named:"Windy")
+            }
+        }
+        else{
+            return nil
+        }
+    }
 }
---
+++ Weatherify/Model/WeatherManager.swift
@@ -0,0 +1,69 @@
+//
+//  WeatherManager.swift
+//  Weatherify
+//
+//  Created by Daniel Wahby on 01/03/2022.
+//
+
+import Foundation
+import Alamofire
+import CoreLocation
+
+protocol WeatherManagerDelegate {
+    func didUpdateWeather(_ weatherManager: WeatherManager, weather: Weather)
+    func didFailWithError(error: Error)
+}
+
+
+struct WeatherManager {
+    
+    // MARK: Define API base URL & APIKey
+    var API_Key = "b7b94d47b557fe41ed9220a9a6140e9b"
+    var baseURL = "http://api.weatherstack.com/current?access_key=" {
+        didSet{}
+    }
+    
+    var weather = Weather()
+    
+    var delegate : WeatherManagerDelegate?
+    
+    func getWeatherConditions(long:CLLocationDegrees,lat:CLLocationDegrees,units:Character) {
+        
+        //                debugPrint(response)
+        DispatchQueue.main.async {
+            let urlString = "\(baseURL)\(API_Key)&query=\(long),\(lat)&units=\(units)"
+            AF.request(urlString ,method: .get,encoding: JSONEncoding.default).responseData { response in
+                switch(response.result) {
+                case.success(let data):
+                    parseJSON(data)
+                case .failure:
+                    if let responseError = response.error {
+                        delegate?.didFailWithError(error: responseError)
+                    }
+                    break
+                }
+            }
+        }
+    }
+    
+    func parseJSON(_ data:Data){
+        _ = JSONDecoder()
+        do {
+            let weatherData = try JSONDecoder().decode( Weather.self, from: data)
+            delegate?.didUpdateWeather(self, weather: weatherData)
+        } catch {
+            delegate?.didFailWithError(error: error)
+        }
+    }
+    
+    func getCurrentUnit(selectedUnitIndex: Int)->Character{
+        switch selectedUnitIndex {
+        case 0: return "m"
+        case 1: return "f"
+        case 2: return "s"
+        default: return "m"
+        }
+    }
+    
+   
+}
